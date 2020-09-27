ec07a2bca367e528f164775fdc44137926211d96
Biimok <jeanlucxdwow@gmail.com>
2020-09-27 19:19:38 -0300

    erick mexendo

--- ecommerce/src/components/Modal/index.js
+++ ecommerce/src/components/Modal/index.js
@@ -7,7 +7,7 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import EditIcon from '@material-ui/icons/Edit';
-import Produto from '../../pages/Listas/Produto';
+import Produto from '../../pages/Listas/Produto/index';
 
 const FormDialog = () => {
     const [open, setOpen] = React.useState(false);
